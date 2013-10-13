f10ae7173c4ad79422d5cbd63de83f29cf79818f
meganemura <mura2megane@gmail.com>
2013-10-13 23:38:04 +0900

    Add recipe 03

--- recipes/app/recipes/03/hello_world_layer.rb
+++ recipes/app/recipes/03/hello_world_layer.rb
@@ -2,4 +2,17 @@ class HelloWorldLayer < Joybox::Core::Layer
 
   scene
 
+  def self.new
+    player = Sprite.new(
+      :file_name => 'recipes/recipe03/monkey01.png',
+      :rect => [[0, 0], [100, 135]]
+    )
+    player.position = [player.contentSize.width.half * 3, Screen.half_height]
+    player.tag = 2
+    layer = super
+    layer << player
+
+    layer
+  end
+
 end
