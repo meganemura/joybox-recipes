13c279c10bf60fa106b0412b5dcd34ec87ab814a
meganemura <mura2megane@gmail.com>
2013-10-19 22:39:42 +0900

    Bug fix: recipe.sh

--- recipes/recipe.sh
+++ recipes/recipe.sh
@@ -3,4 +3,6 @@ set -e
 PROJECT=$1
 motion create --template=joybox-ios $PROJECT
 rm -rf "$PROJECT/resources"
-ln -s ../resources "$PROJECT/resources"
+ln -s ../../resources "$PROJECT/resources"
+git add "$PROJECT"
+git commit -m"Recipe $PROJECT: Initial"
